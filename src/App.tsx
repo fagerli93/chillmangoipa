b56b1e8b8adc2864604777226642f1a8439314d0
Simon Fagerli <fagerli93@live.no>
2021-06-11 10:04:13 +0200

    Added inputfield and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const handleOnClick = () => {
+    alert(`Du søkte på: ${inputText}`);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          value={inputText}
+          onChange={(text) => setInputText(text.target.value)}
+          placeholder="Hvilken fylke vil du sjekke kvadratmeterprisen"
+        />
+        <button onClick={handleOnClick}>Søk!</button>
       </header>
     </div>
   );
